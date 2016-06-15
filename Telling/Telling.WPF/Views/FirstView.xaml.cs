92b980d750499d652c25488ca62e585c9e3245bf
Werner Visser <werner.m.visser@gmail.com>
2016-06-15 15:11:02 +0200

    Add project files.

---
+++ Telling/Telling.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace Telling.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
