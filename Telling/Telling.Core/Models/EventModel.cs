fc52277b9713873086c1c9584c90b8e1badc650f
Werner Visser <werner.m.visser@gmail.com>
2016-08-24 16:09:59 +0200

    pre dev prep

---
+++ Telling/Telling.Core/Models/EventModel.cs
@@ -0,0 +1,14 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telling.Core.Models
+{
+    //public class EventModel : RealmObject
+    //{
+    //    public string Title { get; set; }
+    //}
+}
---
+++ Telling/Telling.iOS/Views/EventListingView.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Telling.Core.ViewModels;
+
+namespace Telling.iOS.Views
+{
+    public class EventListingView : MvxViewController<EventListingViewModel>
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<EventListingView, EventListingViewModel>();
+            set.Apply();
+
+        }
+    }
+}
