ab226a50d29675dd78624dc651f6962fe435c5e0
Werner Visser <werner.m.visser@gmail.com>
2016-09-28 16:04:56 +0200

    updated provisioningprofile configs

--- Telling/Telling.iOS/Views/Cells/SessionListingItemCell.cs
+++ Telling/Telling.iOS/Views/Cells/SessionListingItemCell.cs
@@ -32,58 +32,58 @@ namespace Telling.iOS.Views.Cells
 
         void CreateLayout()
         {
-            Icon = new TCorneredImageView(UIImage.FromBundle("images/missing.png"));
-            ContentView.AddSubviews(Icon);
+            //Icon = new TCorneredImageView(UIImage.FromBundle("images/missing.png"));
+            //ContentView.AddSubviews(Icon);
 
-            Title = new TLabelView();
-            ContentView.AddSubviews(Title);
+            //Title = new TLabelView();
+            //ContentView.AddSubviews(Title);
 
-            SubTitle = new TLabelView();
-            UIFont font = SubTitle.Font;
-            SubTitle.Font = font.WithSize(10f);
-            ContentView.AddSubviews(SubTitle);
+            //SubTitle = new TLabelView();
+            //UIFont font = SubTitle.Font;
+            //SubTitle.Font = font.WithSize(10f);
+            //ContentView.AddSubviews(SubTitle);
 
-            Seperator = TView.MakeSeperator();
-            ContentView.AddSubviews(Seperator);
+            //Seperator = TView.MakeSeperator();
+            //ContentView.AddSubviews(Seperator);
 
-            CenterRuler = TView.MakeSeperator(UIColor.Clear);
-            ContentView.AddSubviews(CenterRuler);
+            //CenterRuler = TView.MakeSeperator(UIColor.Clear);
+            //ContentView.AddSubviews(CenterRuler);
 
-            ContentView.AddConstraints(new FluentLayout[] {
+            //ContentView.AddConstraints(new FluentLayout[] {
 
-                CenterRuler.WithSameCenterY(ContentView),
+            //    CenterRuler.WithSameCenterY(ContentView),
 
-                Icon.AtLeftOf(ContentView, Constants.Margin),
-                Icon.WithSameCenterY(ContentView),
-                Icon.Height().EqualTo(40f),
-                Icon.Width().EqualTo(40f),
+            //    Icon.AtLeftOf(ContentView, Constants.Margin),
+            //    Icon.WithSameCenterY(ContentView),
+            //    Icon.Height().EqualTo(40f),
+            //    Icon.Width().EqualTo(40f),
 
-                Title.Above(CenterRuler),
-                Title.ToRightOf(Icon, Constants.Margin),
-                //Title.Width().EqualTo(ContentView.Frame.Size.Width)
+            //    Title.Above(CenterRuler),
+            //    Title.ToRightOf(Icon, Constants.Margin),
+            //    //Title.Width().EqualTo(ContentView.Frame.Size.Width)
 
-                SubTitle.Below(CenterRuler),
-                SubTitle.ToRightOf(Icon, Constants.Margin),
+            //    SubTitle.Below(CenterRuler),
+            //    SubTitle.ToRightOf(Icon, Constants.Margin),
 
-                Seperator.AtBottomOf(ContentView),
-                Seperator.Height().EqualTo(1f),
-                Seperator.WithSameWidth(ContentView),
+            //    Seperator.AtBottomOf(ContentView),
+            //    Seperator.Height().EqualTo(1f),
+            //    Seperator.WithSameWidth(ContentView),
 
-            });
+            //});
         }
 
         void InitializeBindings()
         {
-            this.DelayBind(() =>
-            {
-                var set = this.CreateBindingSet<SessionListingItemCell, Session>();
-                set.Bind(Title).To(vm => vm.GameName);//.WithConversion(new StringToNSAttributedStringConverter(), 16.0f);
-                set.Bind(SubTitle).To(vm => vm.SessionDate).WithConversion(new DateTimeToStringConverter());
-                set.Bind(Icon).For(c => c.Image).To(vm => vm.ImageName).WithConversion(new StringToImageConverter());
-                //set.Bind(IconImageView).For(c => c.Image).To(vm => vm.ImageName).WithConversion(new StringToUIImageValueConverter());
-                //set.Bind(NewImage).For(c => c.Hidden).To(vm => vm.New).WithConversion(new VisibleConverter());
-                set.Apply();
-            });
+            //this.DelayBind(() =>
+            //{
+            //    var set = this.CreateBindingSet<SessionListingItemCell, Session>();
+            //    set.Bind(Title).To(vm => vm.GameName);//.WithConversion(new StringToNSAttributedStringConverter(), 16.0f);
+            //    set.Bind(SubTitle).To(vm => vm.SessionDate).WithConversion(new DateTimeToStringConverter());
+            //    set.Bind(Icon).For(c => c.Image).To(vm => vm.ImageName).WithConversion(new StringToImageConverter());
+            //    //set.Bind(IconImageView).For(c => c.Image).To(vm => vm.ImageName).WithConversion(new StringToUIImageValueConverter());
+            //    //set.Bind(NewImage).For(c => c.Hidden).To(vm => vm.New).WithConversion(new VisibleConverter());
+            //    set.Apply();
+            //});
         }
     }
 }
--- Telling/Telling.iOS/Views/Modals/PickerModalView.cs
+++ Telling/Telling.iOS/Views/Modals/PickerModalView.cs
@@ -5,40 +5,40 @@ using System.Text;
 
 namespace Telling.iOS.Views.Modals
 {
-    class PickerModalView : BaseViewController<PickerModalViewModel>, IMvxModalIosView
-    {
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
+    //class PickerModalView : BaseViewController<PickerModalViewModel>, IMvxModalIosView
+    //{
+    //    public override void ViewDidLoad()
+    //    {
+    //        base.ViewDidLoad();
 
-            var bindingSet = this.CreateBindingSet<PickerModalView, PickerModalViewModel>();
-            bindingSet = BindLoader(bindingSet);
+    //        var bindingSet = this.CreateBindingSet<PickerModalView, PickerModalViewModel>();
+    //        bindingSet = BindLoader(bindingSet);
 
-            var exceptionDetails = new TLabelView();
-            Add(exceptionDetails);
+    //        var exceptionDetails = new TLabelView();
+    //        Add(exceptionDetails);
 
-            bindingSet.Bind(exceptionDetails).To(vm => vm.Exception).Apply();
+    //        bindingSet.Bind(exceptionDetails).To(vm => vm.Exception).Apply();
 
-            var closeButton = new UIButton(UIButtonType.Custom);
-            closeButton.SetImage(UIImage.FromBundle("images/close.png"), UIControlState.Normal);
-            closeButton.Frame = new RectangleF(0, 0, 30, 30);
-            NavigationItem.SetLeftBarButtonItems(new UIBarButtonItem[] {
-                new UIBarButtonItem(UIBarButtonSystemItem.FixedSpace, null, null)
-                {
-                    Width = -5f
-                },
-                new UIBarButtonItem(closeButton)
-            }, false);
+    //        var closeButton = new UIButton(UIButtonType.Custom);
+    //        closeButton.SetImage(UIImage.FromBundle("images/close.png"), UIControlState.Normal);
+    //        closeButton.Frame = new RectangleF(0, 0, 30, 30);
+    //        NavigationItem.SetLeftBarButtonItems(new UIBarButtonItem[] {
+    //            new UIBarButtonItem(UIBarButtonSystemItem.FixedSpace, null, null)
+    //            {
+    //                Width = -5f
+    //            },
+    //            new UIBarButtonItem(closeButton)
+    //        }, false);
 
-            bindingSet.Bind(closeButton)
-                .For("TouchUpInside")
-                .To(vm => vm.CloseCommand).Apply();
+    //        bindingSet.Bind(closeButton)
+    //            .For("TouchUpInside")
+    //            .To(vm => vm.CloseCommand).Apply();
 
-            View.AddConstraints(new FluentLayout[] {
-                exceptionDetails.AtTopOf(View, Constants.Margin),
-                exceptionDetails.AtLeftOf(View, Constants.Margin),
-                exceptionDetails.WithSameWidth(View).Minus(Constants.Margin * 2)
-            });
-        }
-    }
+    //        View.AddConstraints(new FluentLayout[] {
+    //            exceptionDetails.AtTopOf(View, Constants.Margin),
+    //            exceptionDetails.AtLeftOf(View, Constants.Margin),
+    //            exceptionDetails.WithSameWidth(View).Minus(Constants.Margin * 2)
+    //        });
+    //    }
+    //}
 }
