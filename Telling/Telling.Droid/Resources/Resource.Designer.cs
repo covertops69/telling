6617ad675b5bdfc9731421554cab0d6a5eca43dc
Werner Visser <werner.m.visser@gmail.com>
2017-03-17 13:18:10 +0200

    added exception modal view and hooked up to viewmodel

--- Telling/Telling.Droid/Resources/Resource.Designer.cs
+++ Telling/Telling.Droid/Resources/Resource.Designer.cs
@@ -2427,8 +2427,8 @@ namespace Telling.Droid
 			// aapt resource value: 0x7f060056
 			public const int expanded_menu = 2131099734;
 			
-			// aapt resource value: 0x7f06007d
-			public const int fab = 2131099773;
+			// aapt resource value: 0x7f06007e
+			public const int fab = 2131099774;
 			
 			// aapt resource value: 0x7f060039
 			public const int fill = 2131099705;
@@ -2442,8 +2442,8 @@ namespace Telling.Droid
 			// aapt resource value: 0x7f06003d
 			public const int @fixed = 2131099709;
 			
-			// aapt resource value: 0x7f06007e
-			public const int gameImageView = 2131099774;
+			// aapt resource value: 0x7f06007f
+			public const int gameImageView = 2131099775;
 			
 			// aapt resource value: 0x7f060005
 			public const int home = 2131099653;
@@ -2526,8 +2526,8 @@ namespace Telling.Droid
 			// aapt resource value: 0x7f060059
 			public const int radio = 2131099737;
 			
-			// aapt resource value: 0x7f06007c
-			public const int recycler_view = 2131099772;
+			// aapt resource value: 0x7f06007d
+			public const int recycler_view = 2131099773;
 			
 			// aapt resource value: 0x7f060033
 			public const int right = 2131099699;
@@ -2655,8 +2655,8 @@ namespace Telling.Droid
 			// aapt resource value: 0x7f060048
 			public const int title = 2131099720;
 			
-			// aapt resource value: 0x7f06007f
-			public const int titleTextView = 2131099775;
+			// aapt resource value: 0x7f06007c
+			public const int titleTextView = 2131099772;
 			
 			// aapt resource value: 0x7f06004d
 			public const int title_template = 2131099725;
@@ -2862,52 +2862,55 @@ namespace Telling.Droid
 			public const int fragment_addsession = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int fragment_sessionlisting = 2130903081;
+			public const int fragment_modal = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int listitem_recycler_view = 2130903082;
+			public const int fragment_sessionlisting = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_media_action = 2130903083;
+			public const int listitem_recycler_view = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_media_cancel_action = 2130903084;
+			public const int notification_media_action = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int notification_template_big_media = 2130903085;
+			public const int notification_media_cancel_action = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int notification_template_big_media_narrow = 2130903086;
+			public const int notification_template_big_media = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int notification_template_lines = 2130903087;
+			public const int notification_template_big_media_narrow = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int notification_template_media = 2130903088;
+			public const int notification_template_lines = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int notification_template_part_chronometer = 2130903089;
+			public const int notification_template_media = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int notification_template_part_time = 2130903090;
+			public const int notification_template_part_chronometer = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int select_dialog_item_material = 2130903091;
+			public const int notification_template_part_time = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int select_dialog_multichoice_material = 2130903092;
+			public const int select_dialog_item_material = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int select_dialog_singlechoice_material = 2130903093;
+			public const int select_dialog_multichoice_material = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int SplashScreen = 2130903094;
+			public const int select_dialog_singlechoice_material = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int support_simple_spinner_dropdown_item = 2130903095;
+			public const int SplashScreen = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int toolbar = 2130903096;
+			public const int support_simple_spinner_dropdown_item = 2130903096;
+			
+			// aapt resource value: 0x7f030039
+			public const int toolbar = 2130903097;
 			
 			static Layout()
 			{
---
+++ Telling/Telling.Droid/Views/Fragments/Modals/ExceptionModalFragment.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Shared.Attributes;
+using Telling.Core.ViewModels;
+using Telling.Core.ViewModels.Modals;
+using MvvmCross.Binding.BindingContext;
+
+namespace Telling.Droid.Views.Fragments.Modals
+{
+    [MvxFragment(typeof(MainViewModel), Resource.Id.content_frame)]
+    [Register("telling.droid.views.fragments.modals.ExceptionModalFragment")]
+    class ExceptionModalFragment : BaseFragment<ModalViewModel>
+    {
+        protected override int FragmentId => Resource.Layout.fragment_modal;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
+
+            var bindingSet = this.CreateBindingSet<ExceptionModalFragment, ModalViewModel>();
+            bindingSet.Apply();
+
+            return view;
+        }
+    }
+}
